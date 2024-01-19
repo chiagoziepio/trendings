3fd9bfcd2e970b99b294c06a34f3ce4b2af6febe
chiagozie pio <nduagubapio@gmail.com>
2024-01-19 15:58:54 -0800

    work on the catergories page

--- src/componentsJSx/PostCategories.jsx
+++ src/componentsJSx/PostCategories.jsx
@@ -8,11 +8,29 @@ const PostCategories = ({posts}) => {
   return (
     <div className="PostCatergories">
       {theCategory.length >= 1 ?(
-        <div className="CategoryContainer">
-          {theCategory.map()}
+        <div className="postContainer">
+          {theCategory.map(category => (
+            <div key={category.id} className="postContent">
+              <div className="thePostAuthorAndCategory">
+                <div className="thePostAuthorBx">
+                  <img src={category.authorimg} className="thePostAuthorImg"/>
+                  <p>{category.authorname}</p>
+                </div>
+                <button className="btn">{category.category}</button>
+              </div>
+              <div>
+                <img src={category.img} className="thePostImg" />
+                <h3 className="thePostTitle">{category.title}</h3>
+                <p className="thePostBody">{category.body}</p>
+                <span className="thePostDate">{category.date}</span>
+              </div>
+            </div>
+          ))}
         </div>
       ):(
-        <div></div>
+        <div className="empty">
+          <p>No posts under {categories} yet</p>
+        </div>
       )}
     </div>
   )
