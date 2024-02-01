bbd7eff5ae361c7a3b3290ac8a2e93bd0d37e3c6
chiagozie pio <nduagubapio@gmail.com>
2024-02-01 03:29:59 -0800

    make some changes

--- src/componentsJSx/Home.jsx
+++ src/componentsJSx/Home.jsx
@@ -4,7 +4,7 @@ import '../componentsCss/Home.css'
 const Home = ({trendings}) => {
   return (
     <section className="home">
-      <h1 className=' home-title'>Trends</h1>
+      <h1 className=' home-title pageTitle'>Trends</h1>
       {trendings.map(trends =>(
         <TrendFeeds
           key= {trends.id}
